15a74f44a2ccbc3197882681ecc8397f3b1e1073
Shirasagi0012 <23011330+Shirasagi0012@users.noreply.github.com>
2026-08-27 04:06:17 +0800

    unicorn: Wire up MiuiCamera
    - inherit miuicamera-unicorn device.mk if present
    - set camera package_name soong config so frameworks/av injects
      Xiaomi vendor tags for com.android.camera
    
    Change-Id: I87d5a9692d95d0fb94302898d608278b1ae2776c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,9 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/xiaomi/unicorn/BoardConfigVendor.mk
 
+# MiuiCamera
+-include device/xiaomi/miuicamera-unicorn/BoardConfig.mk
+
 DEVICE_PATH := device/xiaomi/unicorn
 
 # Audio
